c75e2a3fb39c0738c3efa16e8b1e274ee5d2a151
FlutLab.io <>
2022-05-25 03:18:24 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context1) {
+    return MaterialApp(
+      home: MyGallery(),
+    );
+  }
+}
